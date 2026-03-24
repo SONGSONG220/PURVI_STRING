ed4db85bc6ad751306bf5b3018e0f5e1c862bd57
Song <satypaldhakad0@gmail.com>
2026-03-24 20:33:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Aapki API Details
 API_ID = int(getenv("API_ID", "21803165"))
 API_HASH = getenv("API_HASH", "05e5e695feb30e25bef47484cc006da7")
 
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", "7812646657"))
+# Bot Token jo aapne diya tha
+BOT_TOKEN = getenv("BOT_TOKEN", "8645782241:AAHDC-tfWTqCvFSTFlKT3yMxfdUoh4tj9Hw")
 
-MONGO_DB_URI = getenv("mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# Owner aur Channel details
+OWNER_ID = int(getenv("OWNER_ID", "7812646657"))
 MUST_JOIN = getenv("MUST_JOIN", "botbykilwakillua")
+
+# Database URI
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
